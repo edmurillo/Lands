ae818950c1a460ded270744f8d2f3a3d8ad5d580
edmurillo <edmurillo56@misena.edu.co>
2018-02-12 12:46:29 -0500

    commit

---
+++ Lands/ViewModels/LoginViewModel.cs
@@ -0,0 +1,47 @@
+﻿using System;
+namespace Lands.ViewModels
+{
+    public class LoginViewModel
+    {
+        #region
+        public String Email
+        {
+            get;
+            set;
+        }
+
+        public String Password
+        {
+            get;
+            set;
+        }
+
+        public bool IsRunning
+        {
+            get;
+            set;
+        }
+
+        public String Remembered
+        {
+            get;
+            set;
+        }
+        #endregion
+
+        #region Constructor
+        public Login()
+        {
+           
+        }
+        #endregion
+
+        #region Commands
+        public String Buttoncommand
+        {
+            get;
+            set;
+        }
+        #endregion
+    }
+}
---
+++ Lands/ViewModels/MainViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+namespace Lands.ViewModels
+{
+    public class MainViewModel
+    {
+        #region ViewModel
+        public LoginViewModel Login
+        {
+            get;
+            set;
+        }
+        #endregion
+
+        #region
+        public LoginViewModel MyProperty
+        {
+            get;
+            set;
+        }
+        #endregion
+
+
+    }
+}
